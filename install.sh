93a4bf6774094596c7e89a7b5dd0e7210ed34cd8
Alex Zaporozhets <alexzaporozhets@gmail.com>
2012-10-05 16:44:48 +0300

    refactoring: 1) add repos 2) update the system 3) install

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@
 # PHP 5.4
 # http://www.barryodonovan.com/index.php/2012/05/22/ubuntu-12-04-precise-pangolin-and-php-5-4-again
 
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:ondrej/php5
-
-apt-get update
-apt-get upgrade
-apt-get dist-upgrade
+echo >> /etc/apt/sources.list 
+echo "deb http://ppa.launchpad.net/ondrej/php5/ubuntu precise main"  >> /etc/apt/sources.list
 
 # Percona 5.5
 # http://www.percona.com/doc/percona-server/5.5/installation/apt_repo.html
 
-sudo apt-get install percona-server-server-5.5
-sudo gpg -a --export CD2EFD2A | sudo apt-key add -
+gpg --keyserver  hkp://keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+gpg -a --export CD2EFD2A | sudo apt-key add -
 
 # Add this to /etc/apt/sources.list, replacing VERSION with the name of your distribution
+echo >> /etc/apt/sources.list 
 echo "deb http://repo.percona.com/apt precise main" >>  /etc/apt/sources.list
 echo "deb-src http://repo.percona.com/apt precise main" >>  /etc/apt/sources.list
 
-sudo apt-get update
-sudo apt-get install percona-server-server-5.5 percona-server-client-5.5
+apt-get update --force-yes -y
+apt-get upgrade --force-yes -y
+apt-get dist-upgrade --force-yes -y
+
+apt-get install --force-yes -y nginx percona-server-server-5.5 percona-server-client-5.5 php5-cli php5-fpm
